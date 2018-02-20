0e0cbeb3ba9d5569bebc67cc89d5b9d93be8e34d
OleksandrVovchuk <35272619+OleksandrVovchuk@users.noreply.github.com>
2018-02-20 11:14:13 +0200

    Add files via upload

---
+++ hidden.py
@@ -0,0 +1,5 @@
+def oauth() :
+    return { "consumer_key" : "rssVA1KsPXehSF6KlhWv8XABR",
+        "consumer_secret" : "0CFmlzJbiCPIaGxmyHH8nJse4aITsEYFKuaKd2VMfdeNjEn3tk",
+        "token_key" : "857986853248618496-6h9tzJUGhccRqzd44wf1kLRTCGEKBom",
+        "token_secret" : "OOYK0ghjI5Q7gR7XRjfDZPca2aBLHhZDD3kd6vSCbr2dW" }
